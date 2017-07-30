6bfc5cd9234c18928b4990c265aa5d779fcbf478
Christopher <camenarescoding@gmail.com>
2017-07-30 18:47:36 -0400

    Submit a search to Google

--- src/main/java/tld/domain/my_addon/AskGoogleQuestion.java
+++ src/main/java/tld/domain/my_addon/AskGoogleQuestion.java
@@ -1,12 +1,15 @@
 package tld.domain.my_addon;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 import io.testproject.java.enums.ExecutionResultType;
 import io.testproject.java.sdk.generated.codeblocks.Action;
 
 public class AskGoogleQuestion extends Action {
-
+	
+	private String question = "How can I best test my software?";
 	@Override
 	protected ExecutionResultType execute() throws Exception {
 		// Get Driver
@@ -19,6 +22,15 @@ public class AskGoogleQuestion extends Action {
 			this.setMessage("Are you on Google Search page ?");
 			return ExecutionResultType.Failed;
 		}
+		
+		// Find search box element
+		WebElement searchBox = driver.findElement(By.name("q"));
+		// Clear any previous text
+		searchBox.clear();
+		// Type question
+		searchBox.sendKeys(this.question);
+		// Submit search
+		searchBox.submit();
 		
 		return null;
 		
